17dae3453a95aa323dc066d8fd441cb9b8f82773
German Hernandez <muygrafico@gmail.com>
2016-09-22 11:44:35 -0500

    create basic mvc structure | add add dice button listener

--- application.js
+++ application.js
@@ -1,16 +1,42 @@
-$(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    $('.dice').append('<div class="die">0</div>');
-  });
-
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
-});
-
-
+// $(document).ready(function() {
+//   $('#roller button.add').on('click', function() {
+//     $('.dice').append('<div class="die">0</div>');
+//   });
+//
+//   $('#roller button.roll').on('click', function() {
+//     $('.die').each(function(k, die) {
+//       var value = Math.floor((Math.random()*6)+1);
+//       $(die).text(value);
+//     });
+//   });
+// });
 
 
+// $(document).ready(function() {
+  function Dice(maxSidesParam){
+
+    this.sides = maxSidesParam
+  }
+  function View(){
+    this.dices = 0
+
+  }
+
+  function Controller(view, model){
+    this.view = view
+    this.model = model
+
+  }
+  Controller.prototype = {
+    listenButton: function(){
+      $('.add').click(function(event) {
+        console.log("add clicked")
+      });
+    }
+  };
+  var dice = new Dice(10)
+  var view = new View()
+  var controller = new Controller(view, dice)
+  controller.listenButton()
+  console.log(controller)
+ // });

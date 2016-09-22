55030fad63f9efaff6812cf3ee08660aa3ef2de0
German Hernandez <muygrafico@gmail.com>
2016-09-22 14:26:26 -0500

    kinda works not so OOP

--- application.js
+++ application.js
@@ -1,10 +1,11 @@
 // $(document).ready(function() {
 //   $('#roller button.add').on('click', function() {
-//     $('.dice').append('<div class="die">0</div>');
+//    $('.dice').append('<div class="die">0</div>');
 //   });
-//
+
 //   $('#roller button.roll').on('click', function() {
 //     $('.die').each(function(k, die) {
+//       console.log(k)
 //       var value = Math.floor((Math.random()*6)+1);
 //       $(die).text(value);
 //     });
@@ -12,14 +13,25 @@
 // });
 
 
-// $(document).ready(function() {
+$(document).ready(function() {
   function Dice(maxSidesParam){
-
-    this.sides = maxSidesParam
+    this.sides = maxSidesParam;
   }
-  function View(){
-    this.dices = 0
 
+  Dice.prototype = {
+    roll: function(){
+      // $('.dice').append('<div class="die">0</div>');
+    }
+  }
+
+  function View(){
+    this.dices = 0;
+  }
+
+  View.prototype = {
+    addDices: function(){
+      $('.dice').append('<div class="die">0</div>');
+    }
   }
 
   function Controller(view, model){
@@ -29,13 +41,18 @@
 
   Controller.prototype = {
     listenAddBtn: function(){
+      self = this;
       $('.add').click(function(event) {
-        console.log("add")
+        self.view.addDices()
       })
     },
     listenRollBtn: function(){
+      self = this;
       $('.roll').click(function(event) {
-        console.log("roll")
+        $('.die').each(function(k, die) {
+          var value = Math.floor((Math.random()* self.model.sides )+1);
+          $(die).text(value);
+        });
       })
     }
   };
@@ -46,4 +63,4 @@
   controller.listenAddBtn()
   controller.listenRollBtn()
   console.log(controller)
- // });
+ });

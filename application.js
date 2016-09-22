2615bc8325d79c73d7933b78ba0643c92e6c6512
upgraded-robot <mariadotdev.ln@gmail.com>
2016-09-22 17:12:37 -0500

    Refactor OOP de Angie y Maria

--- application.js
+++ application.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $('#roller button.add').on('click', function() {
     $('.dice').append('<div class="die">0</div>');
   });
@@ -10,7 +10,44 @@ $(document).ready(function() {
     });
   });
 });
+*/
+$(document).ready(function(){
+	function randomNumber(){
+		return Math.floor((Math.random()*6)+1);
+	}
+	
+	function Dice(value){
+		this.value = value
+	}
+	
+	Dice.prototype.roll = function(){
+		$('.die').each(function(k, die) {
+			var anotherValue = randomNumber();
+			$(die).text(anotherValue);
+		});
+	}
+	
+	function Player (){}
 
-
+	Player.prototype.add = function(){
+		$('#roller button.add').on('click', function() {
+			$('.dice').append('<div class="die">0</div>');
+		});
+	}
+	
+	Player.prototype.rollListener = function(newDice){
+		$('#roller button.roll').on('click', function() {
+			newDice.roll();
+		});
+	} 
+	
+	function init(){
+		var player = new Player();
+		die = new Dice();
+		player.add();
+		player.rollListener(die);
+	}  
+	init();
+});
 
 
